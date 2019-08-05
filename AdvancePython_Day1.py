f9df957523a747bcbe533334c0b5b029360612ab
kavyakammaripalle <46981255+kavyakammaripalle@users.noreply.github.com>
2019-08-04 20:23:55 -0400

    Add files via upload

---
+++ AdvancePython_Day1.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+#Numpy-numerical computing package
+#installation
+get_ipython().system('pip install numpy')
+
+
+# In[5]:
+
+
+#To upgrade
+get_ipython().system('pip install numpy--upgrade')
+
+
+# In[8]:
+
+
+import numpy as np
+array1=np.array([1,2,3,4,5])
+array1
+
+
+# In[9]:
+
+
+numbers=[9,8,7,6]
+array2=np.array(numbers)
+array2
+
+
+# In[12]:
+
+
+#Array of zeros
+array_of_zeros=np.zeros([5,4])
+array_of_zeros
+
+
+# In[ ]:
+
+
+
+
